ee85a4a0db7d0ae59a509e9ec1d670d77ed8ee9c
maoly <maoly@ngarihealth.com>
2021-01-13 10:08:22 +0800

    Initial commit

---
+++ src/main/java/com/ngari/spring_annotation/SpringAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.ngari.spring_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnnotationApplication.class, args);
+    }
+
+}
